8ccd8288fcfcffd31d2162f18338a6cd5a9a9ceb
Qiang Li <qiang4.li@ge.com>
2018-04-27 17:38:14 -0700

    set up services

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# docker-dev
 # docker-predix
--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ set -x
 #docker pull asperitus/predix
 
 #
-docker run -it --rm --privileged --name predix-dev -p 8080:8080 -v /dev/shm:/dev/shm --entrypoint bash asperitus/predix
+docker run -it --rm --privileged --name predix-dev -p 8080:8080 -v /dev/shm:/dev/shm --network predix-net --entrypoint bash asperitus/predix
